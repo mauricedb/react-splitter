3180ec79ba1f494cc334933b5c5ef5ba24cca5e4
Maurice de Beijer <mauricedb@computer.org>
2015-06-28 12:06:47 +0200

    Basic gulp task

---
+++ dist/react-splitter.js
@@ -0,0 +1,11 @@
+var Splitter = React.createClass({
+    displayName: "Splitter",
+
+    render: function render() {
+        return React.createElement(
+            "div",
+            null,
+            "React Splitter"
+        );
+    }
+});
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Maurice on 6/28/2015.
+ */
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('compile', function () {
+    return gulp.src('src/*.jsx')
+        .pipe(babel({
+            blacklist: [
+                'useStrict'
+            ]
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch-js', function () {
+    gulp.watch('src/*.jsx', ['compile'])
+});
+
+gulp.task('default', ['compile', 'watch-js']);
